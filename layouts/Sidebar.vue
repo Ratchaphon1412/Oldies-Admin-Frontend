e95ba5088d4e7726a73228c745181fcc3aac99b8
kornavit <kornavit.p@ku.th>
2025-03-31 22:07:44 +0700

    add chat in sidebar

--- layouts/Sidebar.vue
+++ layouts/Sidebar.vue
@@ -1,7 +1,7 @@
 <script setup>
 import {
   Home,
-  // MessageCircleMore,
+  MessageCircleMore,
   // Package,
   // CreditCard,
   // ShoppingCart,
@@ -101,13 +101,14 @@ import {
                         Facilitator
                     </TooltipContent>
                 </Tooltip>
-                </TooltipProvider>
+                </TooltipProvider> -->
 
                 <TooltipProvider>
                 <Tooltip>
                     <TooltipTrigger as-child>
                     <NuxtLink
                         to="https://chatwoot.oldiesth.com/"
+                        target="_blank"
                         class="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
                     >
                         <MessageCircleMore class="h-5 w-5" />
@@ -118,7 +119,7 @@ import {
                         Chat
                     </TooltipContent>
                 </Tooltip>
-                </TooltipProvider> -->
+                </TooltipProvider>
             </nav>
         </aside>
             <slot />
